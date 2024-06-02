ad24c8e4dc1740d404fa870aa60b6e3a12e92747
Artur Barilo <efreyu@gmail.com>
2024-06-02 20:10:36 +0300

    master | weapon component /wip

--- Source/RapidFire/Private/Weapons/RFBaseWeapon.cpp
+++ Source/RapidFire/Private/Weapons/RFBaseWeapon.cpp
@@ -7,6 +7,7 @@
 
 ARFBaseWeapon::ARFBaseWeapon()
     : SkeletalMeshComponent(CreateDefaultSubobject<USkeletalMeshComponent>("SkeletalMeshComponent"))
+    , MuzzleSocketName(RapidFire::Constants::Socket::MuzzleSocket)
 {
     PrimaryActorTick.bCanEverTick = false;
     SetRootComponent(SkeletalMeshComponent);
@@ -28,5 +29,20 @@ void ARFBaseWeapon::MakeShot()
 {
     if (!GetWorld())
         return;
-    // FTransform const SocketTransform = SkeletalMeshComponent->GetSocketTransform(RapidFire::Constants::Socket::MuzzleSocket);
+    FTransform const SocketTransform = SkeletalMeshComponent->GetSocketTransform(MuzzleSocketName);
+    FVector const TraceStart = SocketTransform.GetLocation();
+    FVector TraceEnd = TraceStart + SocketTransform.GetRotation().GetForwardVector() * 10000.0f;
+    FHitResult HitResult;
+    GetWorld()->LineTraceSingleByChannel(HitResult, TraceStart, TraceEnd, ECollisionChannel::ECC_Visibility);
+    if (HitResult.bBlockingHit)
+    {
+        DrawDebugSphere(GetWorld(), HitResult.ImpactPoint, 10.0f, 12, FColor::Red, false, 1.0f, 0, 1.0f);
+        TraceEnd = HitResult.ImpactPoint;
+        UE_LOG(LogTemp, Warning, TEXT("Hit: '%s'"), *HitResult.GetActor()->GetName());
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Hit: None"));
+    }
+    DrawDebugLine(GetWorld(), TraceStart, TraceEnd, FColor::Red, false, 1.0f, 0, 1.0f);
 }
--- Source/RapidFire/Public/Weapons/RFBaseWeapon.h
+++ Source/RapidFire/Public/Weapons/RFBaseWeapon.h
@@ -31,4 +31,18 @@ private:
 protected:
     UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
     USkeletalMeshComponent* SkeletalMeshComponent;
+    // todo remove me, first argument:
+    // - EditAnywhere Both the default value set in the Blueprint editor and per-instance values can be edited in the property windows.
+    // - VisibleAnywhere The value can be seen, but not edited, both in the Blueprint editor and per-instance in the Level Editor.
+    // - EditDefaultsOnly The default value can be edited in the Blueprint editor, but not on instances.
+    // - VisibleDefaultOnly The default value is visible in the Blueprint editor, but not on instances.
+    // - EditInstanceOnly The value can be edited on instances, but the default value is locked.
+    // - VisibleInstanceOnly Instance values can be seen, but not edited.
+    // second argument
+    // - BlueprintReadOnly Blueprint Visual Scripts can read, but not write, the property.
+    // - BlueprintReadWrite Blueprint Visual Scripts can both read and write the property.
+    // - EditFixedSize Only for arrays. The elements can be edited, but the number of elements if fixed.
+    // - EditInline Only for pointers. The properties of the pointed-to object is shown instead of the pointer. See more below.
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
+    FName MuzzleSocketName;
 };
