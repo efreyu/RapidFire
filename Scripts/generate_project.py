2bae98dd50e80b3eefbaecf95ae2ef4501594650
Artur Barilo <efreyu@gmail.com>
2024-06-06 20:18:26 +0300

    master | windows fixes

--- Scripts/generate_project.py
+++ Scripts/generate_project.py
@@ -5,14 +5,10 @@ import subprocess
 
 def generate_project(project_dir, config, platform='win32'):
     exec_command = None
-    if platform == 'darwin':
+    if platform == 'darwin' or platform == 'win32':
         # using GenerateProjectFiles.sh
         generate_project_script = config["generate_project_script"].replace("{game_version}", config['game_version'])
         exec_command = f'{generate_project_script} -game -project="{project_dir}/{config["project_name"]}"'
-    elif platform == 'win32':
-        # using UnrealBuildTool.exe
-        ubt_path = os.path.join(config['engine_path'], config['build_tool_relative_path'])
-        exec_command = f'{ubt_path} -projectfiles -project="{project_dir}/{config["project_name"]}" -game -engine'
     elif platform == 'linux':
         # using UnrealBuildTool, not tested yet
         ubt_path = os.path.join(config['engine_path'], config['build_tool_relative_path'])
