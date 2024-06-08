525df6ef225f7f271033635fd9e2db05ebc471e9
Artur Barilo <efreyu@gmail.com>
2024-06-08 12:01:37 +0300

    master | win32 fixes, cleanup ddc script

--- Scripts/cleanup_project.py
+++ Scripts/cleanup_project.py
@@ -2,6 +2,21 @@ import os
 import shutil
 import glob
 
+# Function to remove directories
+def remove_directory(directory):
+    if os.path.exists(directory):
+        try:
+            shutil.rmtree(directory)
+        except Exception as e:
+            print(f"Failed to remove {directory}: {e}")
+        print(f"Removed directory: {directory}")
+
+# Function to remove files
+def remove_files(pattern):
+    for file in glob.glob(pattern):
+        os.remove(file)
+        print(f"Removed file: {file}")
+
 def clean_project(project_dir):
     # Define directories and files to be cleaned
     intermediate_dir = os.path.join(project_dir, 'Intermediate')
@@ -11,21 +26,6 @@ def clean_project(project_dir):
     local_derived_data_cache_dir = os.path.join(project_dir, 'LocalDerivedDataCache')
     saved_dir = os.path.join(project_dir, 'Saved')
     solution_file = os.path.join(project_dir, '*.sln')
-
-    # Function to remove directories
-    def remove_directory(directory):
-        if os.path.exists(directory):
-            try:
-                shutil.rmtree(directory)
-            except Exception as e:
-                print(f"Failed to remove {directory}: {e}")
-            print(f"Removed directory: {directory}")
-
-    # Function to remove files
-    def remove_files(pattern):
-        for file in glob.glob(pattern):
-            os.remove(file)
-            print(f"Removed file: {file}")
 
     # Clean directories and files
     remove_directory(intermediate_dir)
@@ -52,3 +52,17 @@ def remove_temp_files(directory, unused_files=None):
 
         for dir in dirs:
             remove_temp_files(os.path.join(root, dir), unused_files)
+
+def cleanup_ddp_cache(project_dir, unreal_engine_path):
+    derived_data_cache_dir = os.path.join(project_dir, 'DerivedDataCache')
+    remove_directory(derived_data_cache_dir)
+
+    local_derived_data_cache_dir = os.path.join(project_dir, 'LocalDerivedDataCache')
+    remove_directory(local_derived_data_cache_dir)
+
+    home_user_folder = os.path.expanduser("~")
+    appdata_local_unreal_engine_cache = os.path.join(home_user_folder, 'AppData', 'Local', 'UnrealEngine', 'Common', 'DerivedDataCache')
+    remove_directory(appdata_local_unreal_engine_cache)
+
+    unreal_engine_cache = os.path.join(unreal_engine_path, 'Engine', 'DerivedDataCache')
+    remove_directory(unreal_engine_cache)
